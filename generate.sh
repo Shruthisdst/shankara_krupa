30240469e4308dfca837c425cef0b20742c5c986
suresh07 <soori_rozee@yahoo.com>
2015-07-03 18:37:46 +0530

    Commit No. 22 - OCR table created

--- generate.sh
+++ generate.sh
@@ -10,3 +10,4 @@ echo "CREATE DATABASE IF NOT EXISTS $db CHARACTER SET utf8 COLLATE utf8_general_
 perl insert_author.pl $host $db $usr $pwd
 perl insert_feat.pl $host $db $usr $pwd
 perl insert_articles.pl $host $db $usr $pwd
+perl ocr.pl $host $db $usr $pwd
